f69030570ef0387c60ea150aeb371d6de2f1e7d9
wanglurg <wanglurg@gmail.com>
2014-01-28 10:56:57 -0600

    first commit

---
+++ text_read.m
@@ -0,0 +1,8 @@
+function random
+%cd('C:\Users\Junjie\Desktop');
+filename='classeme_keywords.txt';
+
+a = importdata('classeme_keywords.txt',' ', 1);
+%fid=fopen(filename,'r');
+%ff=fscanf(fid,'%s',1)
+
